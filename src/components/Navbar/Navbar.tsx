91c2e16907152521187d9a40fe008b7f2ba3e330
David Simbajon <jdavid32004@gmail.com>
2026-06-18 17:34:31 +0800

    fix: some margin shenanigans

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar({ toggleSidebar, children }: { toggleSidebar?: ()
     <div className="navbar bg-base-100 border-b border-base-300 h-16 w-full z-10 sticky top-0">
       <div className="flex-none">
         {toggleSidebar && (
-          <button 
+          <button
             onClick={toggleSidebar}
             className="btn btn-square btn-ghost"
             aria-label="toggle sidebar"
@@ -36,7 +36,7 @@ export default function Navbar({ toggleSidebar, children }: { toggleSidebar?: ()
           </div>
         )}
       </div>
-      <div className="flex-none gap-2">
+      <div className="flex-none flex items-center gap-3">
         <button className="btn btn-ghost btn-circle">
           <div className="indicator">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
@@ -55,7 +55,6 @@ export default function Navbar({ toggleSidebar, children }: { toggleSidebar?: ()
             <li>
               <a className="justify-between">
                 Profile
-                <span className="badge">New</span>
               </a>
             </li>
             <li><a>Settings</a></li>
